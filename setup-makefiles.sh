f579d386c89098429d6b309368934a07313f2c3e
mnrdnn <udyneos@gmail.com>
2026-03-25 12:39:52 +0700

    update sync

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "fog spes"
+write_headers "RMX2195 RMX2101"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
